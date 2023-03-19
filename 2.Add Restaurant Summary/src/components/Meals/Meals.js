eda54ba592e44b63b9225f8be2696c5871957588
Mohammad Ayub Ansari <mohammadayubansari@Mohammads-MacBook-Air.local>
2023-03-19 12:36:48 +0530

    2. Add Restaurant Summary

---
+++ 2.Add Restaurant Summary/src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import MealsSummary from "./MealsSummary";
+import React from "react";
+
+const Meals = () => {
+  return (
+    <React.Fragment>
+      <MealsSummary />
+    </React.Fragment>
+  );
+};
+
+export default Meals;
